786f4b9e92559c98d527d2950c1ffe3a0a81c002
Your Name <you@example.com>
2023-06-11 11:11:12 +0600

    in the header file

---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+      return (
+            <div>
+                  <h3>This is a Order Review page</h3>
+            </div>
+      );
+};
+
+export default OrderReview;
